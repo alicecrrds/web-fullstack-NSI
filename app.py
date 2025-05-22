f18f277c6a8e3b85440b9a00d0149870ad8fb8e7
alicecrrds <alice.correa-dias@lycee-berthollet.com>
2025-05-22 11:01:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 # Mot de passe en dur
-PASSWORD = "aaa"
+PASSWORD = "indigenas"
 
 @app.route("/")
 def index():
@@ -14,9 +14,11 @@ def check_password():
     data = request.get_json()
     user_input = data.get("password", "")
     if user_input == PASSWORD:
-        return jsonify({"result": "Bravo ! Mot de passe correct."})
+        return jsonify({"result": "isso ae"})
+    elif user_input == "Pedro Alvares Cabral":
+        return jsonify({"result": "MANO NAO. SO NAO."})
     else:
-        return jsonify({"result": "Mot de passe incorrect."})
+        return jsonify({"result": "nuh uh"})
 
 if __name__ == "__main__":
     app.run(debug=True)
